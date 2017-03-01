0d1de73d16872cb43b7d4141d9a726881aa5b584
chen <chenzheng128@yeah.net>
2017-03-01 22:43:51 +0800

    add bufferbloat source

---
+++ sdn/cs144_bufferbloat/bufferbloat_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cd ~
+git clone https://bitbucket.org/huangty/cs144_bufferbloat.git
+sudo apt-get -y install screen
+sudo apt-get -y install python-matplotlib
---
+++ sdn/cs144_bufferbloat/show_figure.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+echo "Use http://localhost:8888/queue.png to see the figure on your browser"
+python -m SimpleHTTPServer 8888
