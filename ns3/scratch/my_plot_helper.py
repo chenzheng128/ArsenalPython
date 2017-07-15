ed5b0b32c89cf51c47591d87c8f1fc516a4a3aef
chen@SourceTree <chenzheng128@yeah.net>
2017-07-16 00:55:35 +0800

    升级了 xgraph.py 可取代 gnuplot

--- ns3/scratch/my_plot_helper.py
+++ ns3/scratch/my_plot_helper.py
@@ -41,9 +41,19 @@ import argparse
 parser = argparse.ArgumentParser(description='对 ns3 tracing 作绘图处理')
 
 # api add_argument() https://docs.python.org/2/library/argparse.html#module-argparse
-parser.add_argument('--merge', action='store_true', help='是否叠加绘图结果')
+parser.add_argument('-m', '--merge', action='store_true', help='是否叠加绘图结果')
+parser.add_argument('-d', '--display', action='store_true', help='是否直接绘图结果')
+parser.add_argument('-f', '--field',
+                    help="选择 field 进行绘图, 默认是前两栏 1:2",
+                    dest="field",
+                    default="1:2")
+parser.add_argument('-l', '--lable',
+                    help="标签名称",
+                    dest="label",
+                    default=None)
 parser.add_argument('filenames', metavar='filename', nargs='+',
                     help='要处理的文件名')
+
 # parser.parse_args(args=['--merge', 'a.txt', 'b.txt'])
 
 args = parser.parse_args()
@@ -53,10 +63,19 @@ args = parser.parse_args()
 #     print "Example: %s ../seventh-cwnd-count.dat" % sys.argv[0]
 #     sys.exit()
 
+print "== ARGUMENT == "
 if args.merge:
     print "== 使用 merge 绘图模式"
 else:
     print "== 使用 独立 绘图模式"
+if args.display:
+    print "== 显示绘图 "
+else:
+    print "== 不显示绘图 "
+
+field_list=args.field.split(":") 
+field1, field2 = int(field_list[0]), int(field_list[1]) 
+print "== 选择 field %d:%d 进行绘图 " % (field1, field2)
 
 plt_handles = []
 for this_file in args.filenames: # support multiple file
@@ -64,18 +83,25 @@ for this_file in args.filenames: # support multiple file
         plt.clf() # 清除上一次绘图
     arr = np.genfromtxt(this_file)
     print ("读入了数据文件 %s" % this_file)
-    this_handle, = plt.plot(arr[:,0], arr[:,1], label=this_file)
+    
+    # 设定输出文件名
+    if not args.label:
+        png_filename = "%s.%s" % (this_file, 'png')
+        args.label = this_file
+    else:
+        png_filename = "%s.%s" % (args.label, 'png')
+    plt.title(args.label)
+    this_handle, = plt.plot(arr[:,field1 - 1], arr[:,field2 - 1], label=args.label)
     if not args.merge:
         plt_handles = [this_handle]
     else:
         plt_handles += [this_handle]
-    plt.title(this_file)
     plt.legend(handles=plt_handles)
     plt.grid(True)
-    png_filename = "%s.%s" % (this_file, 'png')
+        
     plt.savefig(png_filename) # 保存文件
     print ("生成了图形文件 %s" % png_filename)
-    if len(sys.argv) == 2: # 如果仅有一个图表则显示, 否则只是批量生产png文件
+    if args.display: # and len(sys.argv) == 2: # 如果仅有一个图表则显示, 否则只是批量生产png文件
         plt.show()
 
 if args.merge: # merge模式下显示最后的图表
